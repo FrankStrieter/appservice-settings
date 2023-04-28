1acdbb122d8870bf92a2386a32bc80a9d387d915
Frank Strieter <fs@co-IT.eu>
2023-04-28 11:36:02 +0200

    fix variables

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ export async function main() {
         let AppSettings: string = core.getInput('app-settings-json', {required: false});
         let ConnectionStrings: string = core.getInput('connection-strings-json', {required: false});
         let ConfigurationSettings: string = core.getInput('general-settings-json', {required: false});
-	let resourceGroupName: string = core.getInput('group-name', {required: false}) || undefined;
+	let groupName: string = core.getInput('group-name', {required: false}) || undefined;
         const maskInputs: string = core.getInput('mask-inputs', { required: false }).toLowerCase();
         let applicationURL: string;
 
@@ -36,7 +36,7 @@ export async function main() {
         let endpoint: IAuthorizationHandler = await getHandler();
         console.log("Got service connection details for Azure App Service: " + webAppName);
 
-		let appDetails = await AzureResourceFilterUtility.getAppDetails(endpoint, webAppName, resourceGroupName);
+		let appDetails = await AzureResourceFilterUtility.getAppDetails(endpoint, webAppName, groupName);
         let resourceGroupName = appDetails["resourceGroupName"];
 		console.log("Resource Group : "+ resourceGroupName);
 
